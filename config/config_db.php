a81e6ba18057e4ad64176c81c034d422acf9fba8
xiaopengzhu <xp_zhu@qq.com>
2017-10-06 22:07:46 +0800

    001

--- config/config_db.php
+++ config/config_db.php
@@ -1,6 +1,6 @@
 <?php
                    /*
-                   con_db_host = "localhost"
+                   con_db_host = "47.52.90.151"
                    con_db_id   = "root"
                    con_db_pass	= "wwwxpcmsnet"
                    con_db_name = "qwdz"
